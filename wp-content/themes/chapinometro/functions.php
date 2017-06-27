1c1a01e937c7c7771d3a156f1187095cbdc19a44
Rodrigo Kestler <rodrigokestler@gmail.com>
2017-06-27 10:38:26 -0700

    Install plugins wordpress

---
+++ wp-content/themes/chapinometro/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+ * Enqueue scripts and styles.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+function chapinometro_scripts() {
+	
+}
+add_action( 'wp_enqueue_scripts', 'chapinometro_scripts' );
