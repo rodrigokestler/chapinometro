5da68318aa93f3168c267f63f50586424024c101
Rodrigo Kestler <rodrigokestler@gmail.com>
2017-06-28 12:48:22 -0700

    Functions get_niveles

--- wp-content/themes/chapinometro/functions.php
+++ wp-content/themes/chapinometro/functions.php
@@ -59,6 +59,60 @@ function register_user(){
 add_action('wp_ajax_nopriv_register_user','register_user');
 
 function get_niveles(){
-	die('todo bien');
+	$args = array(
+		'posts_per_page'   => -1,
+		'offset'           => 0,
+		'orderby'          => 'title',
+		'order'            => 'DESC',
+		'post_type'        => 'nivel',
+		'post_status'      => 'publish',
+	);
+	$posts_array = get_posts($args);
+	$contador=0;
+	?>
+	<table style="width:100%;text-align:center;">
+	<?php
+	foreach($pots_array as $nivel){
+		$preguntas_acertadas = get_post_meta($nivel->ID,'preguntas_acertadas',TRUE);
+		$url_background="";
+		if($preguntas_acertadas==''){
+			$texto = '';
+			if($nivel->post_title!='1'){
+				$class = "nivel_bloqueado"
+			}else{
+				$class="nivel_juego"
+			}
+		}else{
+			$texto = $preguntas_acertadas+'/10';
+			$class="nivel_juego";
+			$imagen = get_post_meta($nivel->ID,'icono',TRUE);
+			$url_background=$imagen->url;
+		}
+		if($contador==0){ ?>
+			<tr style="height:110px;">
+		<?php }
+		?>
+		
+                        
+                            <td>
+                                <button data-nivelid="<?php echo $nivel->ID; ?>" style="background-image:('<?php echo $url_background; ?>')"class="<?php echo $class;?>">
+                                	<div class="texto_niveles font-morado">
+                                		<?php  echo $texto;?>
+                                	</div>
+                                </button>
+                            </td>
+                       
+         
+        <?php
+        	$contador++;
+        if($contador==3){ ?>
+        	 </tr>
+        <?php 
+          	 $contador=0;
+    	}
+	}
+	?>
+	</table>
+	<?php
 }
 add_action('wp_ajax_nopriv_get_niveles','get_niveles');
