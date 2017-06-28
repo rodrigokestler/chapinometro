ac20dc02baafb0918746ef2dc3c5dfefa3d29c84
Rodrigo Kestler <rodrigokestler@gmail.com>
2017-06-28 13:08:31 -0700

    get niveles

---
+++ wp-content/themes/chapinometro/page-prueba.php
@@ -0,0 +1,60 @@
+<?php
+$args = array(
+        'posts_per_page'   => -1,
+        'offset'           => 0,
+        'orderby'          => 'title',
+        'order'            => 'ASC',
+        'post_type'        => 'nivel',
+        'post_status'      => 'publish',
+    );
+    $posts_array = get_posts($args);
+    $contador=0;
+    ?>
+    <table style="width:100%;text-align:center;">
+    <?php
+    foreach($posts_array as $nivel){
+        $preguntas_acertadas = get_post_meta($nivel->ID,'preguntas_acertadas',TRUE);
+        $url_background="";
+        if($preguntas_acertadas==''){
+            $texto = '';
+            if($nivel->post_title!='1'){
+                $class = "nivel_bloqueado";
+            }else{
+                $class="nivel_juego";
+                $imagen = get_post_meta($nivel->ID,'icono',TRUE);
+                var_dump($imagen);
+                $url_background=$imagen->url;
+            }
+        }else{
+            $texto = $preguntas_acertadas+'/10';
+            $class="nivel_juego";
+            $imagen = get_post_meta($nivel->ID,'icono',TRUE);
+            $url_background=$imagen->url;
+        }
+        if($contador==0){ ?>
+            <tr style="height:110px;">
+        <?php }
+        ?>
+        
+                        
+                            <td>
+                                <button data-nivelid="<?php echo $nivel->ID; ?>" style="background-image:('<?php echo $url_background; ?>')"class="<?php echo $class;?>">
+                                    <div class="texto_niveles font-morado">
+                                        <?php  echo $texto;?>
+                                    </div>
+                                </button>
+                            </td>
+                       
+         
+        <?php
+            $contador++;
+        if($contador==3){ ?>
+             </tr>
+        <?php 
+             $contador=0;
+        }
+    }
+    ?>
+    </table>
+    <?php
+    die();
